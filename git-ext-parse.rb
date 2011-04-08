cf82b30db5664f36987ea48ba97412540ade4cf7
Dave Bacher <drbacher@alum.mit.edu>
2011-04-07 23:47:55 -0700

    separated parsing code for testing, added test cases

--- git-ext-parse.rb
+++ git-ext-parse.rb
@@ -1,25 +1,11 @@
+# Separate parsing, url resolution from ExternalProcesser
 
-old_input = "# /
-third-party/sounds             http://svn.example.com/repos/sounds
-third-party/skins -r148        http://svn.example.com/skinproj
-third-party/skins/toolkit -r21 http://svn.example.com/skin-maker
-"
-
-input = "
-# /
-/^/trunk/tools/NTService@42476  ext-tools/NTService
-/^/trunk/shared/build@42751  shared/build
-/^/trunk/shared/include/cxxtest@42751  shared/include/cxxtest
-"
-
-
-def parse_externals(input)
+def parse_externals(input, repo)
   externals = []
   # externals should contain an array of 
   # hashes with local_dir, url and rev elements
-  input.split("\n").each{ |l|
+  input.each { |l|
     # TODO: get rid of empty lines, comments and '# /'
-    #l.reject { |x| x =~ %r%^\s*/?\s*#% } # get rid of / 
 
     rev = nil
     # strip the dash rev
@@ -32,37 +18,24 @@ def parse_externals(input)
       rev = $~[1]
       l.sub!($~[0], '')
     end
-    
-    local_dir = ""
-    url = ""
-    
-    # versioned_externals = l.grep(/-r\d+\b/i)
-    #     unless versioned_externals.empty?
-    #       print "Error: Found external(s) pegged to fixed revision: '#{versioned_externals.join ', '}' in '#{Dir.getwd}', don't know how to handle this."
-    #     end
-    # try to extract @\d+ from the first element
 
-    if ! l.grep(%r%^/(\S+)\s+(\S+)%).empty? then
-      externals += [ { :dir=> $~[2], :url=> $~[1], :rev=> rev } ]
+    if m = %r|^/\s*(\S+)\s+(\S+)| then
+      url = resolve_url(m[1], "DUMMY", repo)
+      externals += [ { :dir=> $~[2], :url=> url, :rev=> rev } ]
     end
   }
-  
-  # ext += [ rev ]
+
   return externals
 end
 
-def resolve_url(url, parent_url, repo)
-  # TODO: replace ^ with repo
-  url.sub!(/^\^/, repo)
+def resolve_url(partial_url, parent_url, repo)
+  # certain special characters in the externals url need to be expanded
+  # replace ^ with repository url
+  url = partial_url.sub(/^\^/, repo)
   # TODO: replace .. with parent dir
-  # TODO: replace // with (something)
-  # TODO: replace / with (something)
+  # TODO: replace // with repo scheme://
+  # parsed_url = URI.parse(repo)
+  # sch = parsed_url.scheme
+  # TODO: replace / with repo server
   return url
 end
-
-result = parse_externals input
-result.each_with_index { |el, idx| puts "#{idx}: "; el.each { |k,v| puts "#{k}: #{v}" } }
-
-#result = parse_externals old_input
-#result.each_with_index { |el, idx| puts "#{idx}: "; el.each { |k,v| puts "#{k}: #{v}" } }
-
--- test_ext_parse.rb
+++ test_ext_parse.rb
@@ -10,6 +10,23 @@ require 'git-ext-parse'
 #third-party/skins/toolkit -r21 http://svn.example.com/skin-maker
 #"
 
+TEST_CASES = [
+  { :input => "/^/trunk/tools@4001  ext-src/tools", 
+    :repo => "http://example.com/svn",
+    :exp_url => "http://example.com/svn/trunk/tools", 
+    :exp_dir => "ext-src/tools", :exp_rev => "4001" },
+    
+  { :input => "/https://example.com/svn/trunk/tools  ext-src/tools", 
+    :repo => "https://example.com/svn",
+    :exp_url => "https://example.com/svn/trunk/tools", 
+    :exp_dir => "ext-src/tools", :exp_rev => nil },
+
+  { :input => "/-r 3001 ^/trunk/tools/A  ext-src/A", 
+    :repo => "https://example.com/svn",
+    :exp_url => "https://example.com/svn/trunk/tools", 
+    :exp_dir => "ext-src/A", :exp_rev => "3001" },
+]
+
 INPUT = "
 # /
 /^/trunk/tools/NTService@42476  ext-tools/NTService
@@ -18,27 +35,44 @@ INPUT = "
 "
 
 class TestExtParse < Test::Unit::TestCase
-     # def setup
-     # end
+  # def setup
+  # end
 
-     # def teardown
-     # end
+  # def teardown
+  # end
 
-     def test_input
-       result = parse_externals(INPUT)
-       puts result
-       assert_equal(3, result.length)
-       # TODO: check output!
-       assert(false)
-     end
+  def test_invalid_input
+    result = parse_externals("# /", "DUMMY")
+    assert_equal(0, result.length)
+  end       
+
+  def test_inputs
+    TEST_CASES.each do |tc|
+      result = parse_externals(tc[:input], tc[:repo])
+      puts tc[:input]
+      puts result.length, result
+      assert_equal(1, result.length)
+      assert_equal(tc[:exp_url], result[0][:url])
+      assert_equal(tc[:exp_dir], result[0][:dir])
+      assert_equal(tc[:exp_rev], result[0][:rev])
+    end
+  end
+
+  def test_basic_input
+    result = parse_externals(INPUT, "DUMMY")
+    puts result
+    assert_equal(3, result.length)
+    # TODO: check output!
+    
+  end
 end
 
 
 # TODO: test cases for url resolution
 
 class TestUrlResolution < Test::Unit::TestCase
-     def test_caret
-       result = resolve_url("^/dirA/dirB", "dummy", "https://example.com/svn")
-       assert_equal("https://example.com/svn/dirA/dirB", result)
-     end
+  def test_caret
+    result = resolve_url("^/dirA/dirB", "dummy", "https://example.com/svn")
+    assert_equal("https://example.com/svn/dirA/dirB", result)
+  end
 end
